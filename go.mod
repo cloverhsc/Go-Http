3d93feb162ac0ca24565d4506a32a91f1ce1e46e
clover <clover.hsc@gmail.com>
2023-07-19 22:40:50 +0800

    1. add gorilla router management. 2. add static file management. 3. add GET /api/test API function.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example/http
 
 go 1.20
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,56 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+type TestResp struct {
+	Result string `json:"result"`
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	// use default http router
+	/* http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my website!")
 	})
 
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.ListenAndServe(":8080", nil)
+	*/
+
+	// use Gorilla router
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my website!")
+	})
+
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+
+	r.HandleFunc("/api/test", testResponse).Methods("GET")
+	http.ListenAndServe(":8080", r)
+}
+
+/*
+	  API /api/test response json format as below.
+		@result: success
+		@method: GET
+*/
+func testResponse(w http.ResponseWriter, r *http.Request) {
+	resp := TestResp{Result: "success"}
+
+	json.NewEncoder(w).Encode(resp)
+
 }
